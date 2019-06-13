3c2632083900f627b7bf67e2cdce50bec66018fb
HaraKeisuke <mtnnnnnnn@gmail.com>
2019-06-13 17:45:29 +0900

    微修正

--- main.js
+++ main.js
@@ -1,66 +1,73 @@
-window.onload = e => {
-  liff.init(data => {
+window.onload = function(e) {
+  console.log('ロード');
+  liff.init(function(data) {
     initializeApp(data);
   });
 };
 
-const initializeApp = data => {
+function initializeApp(data) {
   // 公式サイトを開く
   document
     .getElementById('openwindowbutton')
-    .addEventListener('click', openURL('https://www.epicgames.com/fortnite'));
+    .addEventListener('click', function() {
+      liff.openWindow({
+        url: 'https://www.epicgames.com/fortnite',
+      });
+    });
 
   document
     .getElementById('close')
     .addEventListener('click', liff.closeWindow());
 
   // 落下地点をランダムで決めて落とす
-  document.getElementById('decideFallPoint').addEventListener('click', () => {
-    // liff.closeWindow();
-    var ary = [
-      'ジャンク・ジャンクション',
-      'ブロック',
-      'レイジー・ラグーン',
-      'サニー・ステップス',
-      'ロンリー・ロッジ',
-      'ホーンテッド・ヒルズ',
-      'プレザント・パーク',
-      'ルート・レイク',
-      'スノビー・ショア',
-      'ティルテッド・タワー',
-      'ダスティ・ディボット',
-      'リテイル・ロー',
-      'シフティ・シャフト',
-      'ソルティ・スプリングス',
-      'フェイタル・フィールド',
-      'パラダイス・パームズ',
-      'ラッキー・ランディング',
-      'ハッピー・ハムレット',
-      'ポーラー・ピーク',
-      'フロスティ・フロイト',
-    ];
-    liff
-      .sendMessages([
-        {
-          type: 'text',
-          text: '今回降りるべき場所は....',
-        },
-        {
-          type: 'text',
-          text: '『 ' + ary[Math.floor(Math.random() * ary.length)] + ' 』です！',
-        },
-      ])
-      .then(function() {
-        window.alert('Message sent');
-      })
-      .catch(function(error) {
-        window.alert('Error sending message: ' + error);
-      });
-  });
-};
+  document
+    .getElementById('decideFallPoint')
+    .addEventListener('click', function() {
+      // liff.closeWindow();
+      var ary = [
+        'ジャンク・ジャンクション',
+        'ブロック',
+        'レイジー・ラグーン',
+        'サニー・ステップス',
+        'ロンリー・ロッジ',
+        'ホーンテッド・ヒルズ',
+        'プレザント・パーク',
+        'ルート・レイク',
+        'スノビー・ショア',
+        'ティルテッド・タワー',
+        'ダスティ・ディボット',
+        'リテイル・ロー',
+        'シフティ・シャフト',
+        'ソルティ・スプリングス',
+        'フェイタル・フィールド',
+        'パラダイス・パームズ',
+        'ラッキー・ランディング',
+        'ハッピー・ハムレット',
+        'ポーラー・ピーク',
+        'フロスティ・フロイト',
+      ];
+      liff
+        .sendMessages([
+          {
+            type: 'text',
+            text: '今回降りるべき場所は....',
+          },
+          {
+            type: 'text',
+            text: '『 ' + ary[Math.floor(Math.random() * ary.length)] + ' 』です！',
+          },
+        ])
+        .then(function() {
+          window.alert('Message sent');
+        })
+        .catch(function(error) {
+          window.alert('Error sending message: ' + error);
+        });
+    });
+}
 
-const openURL = url => {
+function openURL(url) {
   liff.openWindow({
     url: url,
   });
-};
+}
