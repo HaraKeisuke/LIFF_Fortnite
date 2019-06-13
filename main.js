b36aac541fe9be4573a77dfb22f77fced72bbaa4
HaraKeisuke <mtnnnnnnn@gmail.com>
2019-06-13 19:31:01 +0900

    一回戻す

--- main.js
+++ main.js
@@ -1,161 +1,72 @@
-window.onload = function (e) {
-  liff.init(function (data) {
-      initializeApp(data);
+window.onload = function(e) {
+  liff.init(function(data) {
+    initializeApp(data);
   });
 };
 
 function initializeApp(data) {
-  document.getElementById('languagefield').textContent = data.language;
-  document.getElementById('viewtypefield').textContent = data.context.viewType;
-  document.getElementById('useridfield').textContent = data.context.userId;
-  document.getElementById('utouidfield').textContent = data.context.utouId;
-  document.getElementById('roomidfield').textContent = data.context.roomId;
-  document.getElementById('groupidfield').textContent = data.context.groupId;
-
-  // openWindow call
-  document.getElementById('openwindowbutton').addEventListener('click', function () {
+  // 公式サイトを開く
+  document
+    .getElementById('openwindowbutton')
+    .addEventListener('click', function() {
       liff.openWindow({
-          url: 'https://line.me'
+        url: 'https://www.epicgames.com/fortnite',
       });
-  });
+    });
 
-  // closeWindow call
-  document.getElementById('closewindowbutton').addEventListener('click', function () {
-      liff.closeWindow();
-  });
+  document
+    .getElementById('close')
+    .addEventListener('click', liff.closeWindow());
 
-  // sendMessages call
-  document.getElementById('sendmessagebutton').addEventListener('click', function () {
-      liff.sendMessages([{
-          type: 'text',
-          text: "You've successfully sent a message! Hooray!"
-      }, {
-          type: 'sticker',
-          packageId: '2',
-          stickerId: '144'
-      }]).then(function () {
-          window.alert("Message sent");
-      }).catch(function (error) {
-          window.alert("Error sending message: " + error);
-      });
-  });
-
-  // get access token
-  document.getElementById('getaccesstoken').addEventListener('click', function () {
-      const accessToken = liff.getAccessToken();
-      document.getElementById('accesstokenfield').textContent = accessToken;
-      toggleAccessToken();
-  });
-
-  // get profile call
-  document.getElementById('getprofilebutton').addEventListener('click', function () {
-      liff.getProfile().then(function (profile) {
-          document.getElementById('useridprofilefield').textContent = profile.userId;
-          document.getElementById('displaynamefield').textContent = profile.displayName;
-
-          const profilePictureDiv = document.getElementById('profilepicturediv');
-          if (profilePictureDiv.firstElementChild) {
-              profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
-          }
-          const img = document.createElement('img');
-          img.src = profile.pictureUrl;
-          img.alt = "Profile Picture";
-          profilePictureDiv.appendChild(img);
-
-          document.getElementById('statusmessagefield').textContent = profile.statusMessage;
-          toggleProfileData();
-      }).catch(function (error) {
-          window.alert("Error getting profile: " + error);
-      });
-  });
+  // 落下地点をランダムで決めて落とす
+  document
+    .getElementById('decideFallPoint')
+    .addEventListener('click', function() {
+      // liff.closeWindow();
+      var ary = [
+        'ジャンク・ジャンクション',
+        'ブロック',
+        'レイジー・ラグーン',
+        'サニー・ステップス',
+        'ロンリー・ロッジ',
+        'ホーンテッド・ヒルズ',
+        'プレザント・パーク',
+        'ルート・レイク',
+        'スノビー・ショア',
+        'ティルテッド・タワー',
+        'ダスティ・ディボット',
+        'リテイル・ロー',
+        'シフティ・シャフト',
+        'ソルティ・スプリングス',
+        'フェイタル・フィールド',
+        'パラダイス・パームズ',
+        'ラッキー・ランディング',
+        'ハッピー・ハムレット',
+        'ポーラー・ピーク',
+        'フロスティ・フロイト',
+      ];
+      liff
+        .sendMessages([
+          {
+            type: 'text',
+            text: '今回降りるべき場所は....',
+          },
+          {
+            type: 'text',
+            text: '『 ' + ary[Math.floor(Math.random() * ary.length)] + ' 』です！',
+          },
+        ])
+        .then(function() {
+          window.alert('Message sent');
+        })
+        .catch(function(error) {
+          window.alert('Error sending message: ' + error);
+        });
+    });
 }
 
-function toggleAccessToken() {
-  toggleElement('accesstokendata');
+function openURL(url) {
+  liff.openWindow({
+    url: url,
+  });
 }
-
-function toggleProfileData() {
-  toggleElement('profileinfo');
-}
-
-function toggleElement(elementId) {
-  const elem = document.getElementById(elementId);
-  if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
-      elem.style.display = "none";
-  } else {
-      elem.style.display = "block";
-  }
-}
-
-// window.onload = function(e) {
-//   liff.init(function(data) {
-//     initializeApp(data);
-//   });
-// };
-
-// function initializeApp(data) {
-//   // 公式サイトを開く
-//   document
-//     .getElementById('openwindowbutton')
-//     .addEventListener('click', function() {
-//       liff.openWindow({
-//         url: 'https://www.epicgames.com/fortnite',
-//       });
-//     });
-
-//   document
-//     .getElementById('close')
-//     .addEventListener('click', liff.closeWindow());
-
-//   // 落下地点をランダムで決めて落とす
-//   document
-//     .getElementById('decideFallPoint')
-//     .addEventListener('click', function() {
-//       // liff.closeWindow();
-//       var ary = [
-//         'ジャンク・ジャンクション',
-//         'ブロック',
-//         'レイジー・ラグーン',
-//         'サニー・ステップス',
-//         'ロンリー・ロッジ',
-//         'ホーンテッド・ヒルズ',
-//         'プレザント・パーク',
-//         'ルート・レイク',
-//         'スノビー・ショア',
-//         'ティルテッド・タワー',
-//         'ダスティ・ディボット',
-//         'リテイル・ロー',
-//         'シフティ・シャフト',
-//         'ソルティ・スプリングス',
-//         'フェイタル・フィールド',
-//         'パラダイス・パームズ',
-//         'ラッキー・ランディング',
-//         'ハッピー・ハムレット',
-//         'ポーラー・ピーク',
-//         'フロスティ・フロイト',
-//       ];
-//       liff
-//         .sendMessages([
-//           {
-//             type: 'text',
-//             text: '今回降りるべき場所は....',
-//           },
-//           {
-//             type: 'text',
-//             text: '『 ' + ary[Math.floor(Math.random() * ary.length)] + ' 』です！',
-//           },
-//         ])
-//         .then(function() {
-//           window.alert('Message sent');
-//         })
-//         .catch(function(error) {
-//           window.alert('Error sending message: ' + error);
-//         });
-//     });
-// }
-
-// function openURL(url) {
-//   liff.openWindow({
-//     url: url,
-//   });
-// }
